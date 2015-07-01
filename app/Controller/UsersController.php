0f82f3bc7a3a7692a73322e80beec0f03a4e6370
林貴史 <hayashitakashi@rinnkisi-no-MacBook-Air.local>
2015-07-01 23:54:25 +0900

    ユーザの登録機能の実装途中

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -5,15 +5,41 @@
  * @property User $User
  * @property PaginatorComponent $Paginator
  */
+App::uses('AppController', 'Controller');
 class UsersController extends AppController {
 	public $name = 'Users'; //クラス名
-	public $components = array('Session');//
-/**
- * add method
- *
- */
-	function add_user() {
-		$url = $this->api_rest("POST","users.json","",$this->request->data);
+	public $components = array('Session');
+	/*
+	*  ログイン後のトップページ
+	*/
+	function index(){
+		debug($this->request->data);
+		$url = $this->api_rest("POST","logins.json","",$this->request->data);
 		debug($url);
+	}
+
+	/*
+	*  ログイン機能
+	*/
+
+	function login(){
+		debug($this->request->data);
+		if(!empty($this->request->data)){
+			$url = $this->api_rest("POST","users.json","",$this->request->data);
+			debug($url);
+		}
+	}
+	/*
+	*  ユーザ登録機能
+	*/
+	function add_user(){
+		debug($this->request->data);
+		$this->set('default',$this->request->data);
+		if(!empty($this->request->data)){
+			$url = $this->api_rest("POST","users.json","",$this->request->data);
+			debug($url);
+		}else{
+			$this->set('default',array());
+		}
 	}
 }
